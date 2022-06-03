b02b65dba5be8a60a920c8457fca9fe5b6e15908
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-03 17:36:08 -0500

    Carousel React

--- components/Carrousel/Carrousel.js
+++ components/Carrousel/Carrousel.js
@@ -1,61 +1,53 @@
-import Image from 'next/image'
-import {useState, useEffect} from 'react'
-import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
-import styles from './Carousel.module.css'
-
-
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import { Carousel } from "react-responsive-carousel";
+import styles from "./Carousel.module.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function Carrousel() {
-    const ImagesData=[
-        {
-            image:'/images/IMG-9027.png'
-        },
-        {
-            image:'/images/IMG-9029.png'
-    
-        },
-        {
-            image:'/images/IMG-9032.png'
-    
-        },
-        {
-            image:'/images/IMG-9035.png'
-    
-        }
-    ]
-    const [current, setCurrent] = useState(0)
-    const length = ImagesData?.length
 
-    
-    const arrowLeft=()=>{
-        setCurrent(current === 0 ? length - 1: current - 1)
 
-    }
-    const arrowRight=()=>{
-        setCurrent(current === length -1 ? 0 : current + 1)
-    }
-    
-    useEffect(()=>{
-
-    },[current])
-    
   return (
     <>
-    <section className={styles.slider}>
-    <FaArrowAltCircleLeft className={styles.arrowLeft}onClick={arrowLeft}/>
-    <FaArrowAltCircleRight className={styles.arrowRight} onClick={arrowRight}/>
-     {ImagesData?.map((slide, index)=>{
-         return(
-             <div className={index === current ? 'slide active': 'slide'} key={index}>
-             {index === current 
-             && 
-             (<Image src={slide.image} width={200} height={180} alt={index}  className={styles.image}/>)}
-             </div>
-         )
-     })}
-
-
-    </section>
-
+      
+      <Carousel showArrows={true} centerSlidePercentage={60} centerMode={true} showThumbs={false} autoPlay  interval={3000}infiniteLoop={1}>
+        <div >
+          <Image
+            src={"/images/IMG-9027.png"}
+            width={200}
+            height={180}
+            alt={"/images/IMG-9027.png"}
+            className={styles.image}
+          />
+        </div>
+        <div>
+          <Image
+            src={"/images/IMG-9029.png"}
+            width={200}
+            height={180}
+            alt={"/images/IMG-9029.png"}
+            className={styles.image}
+          />
+        </div>
+        <div>
+          <Image
+            src={"/images/IMG-9032.png"}
+            width={200}
+            height={180}
+            alt={"/images/IMG-9032.png"}
+            className={styles.image}
+          />
+        </div>
+        <div>
+          <Image
+            src={"/images/IMG-9035.png"}
+            width={200}
+            height={180}
+            alt={"/images/IMG-9035.png"}
+            className={styles.image}
+          />
+        </div>
+      </Carousel>
     </>
   );
 }
