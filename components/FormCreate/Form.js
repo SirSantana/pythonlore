d2faf5a2f5ddc7c57bc7392cb1fa8c10c67310d8
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-03 23:10:14 -0500

    GapMal

--- components/FormCreate/Form.js
+++ components/FormCreate/Form.js
@@ -1,4 +1,5 @@
 import styles from './Form.module.css'
+import styles1 from '../Navbar/Navbar.module.css'
 import { useState } from 'react'
 const initialForm={
     nombre:'',
@@ -17,16 +18,16 @@ export default function Form(){
         <>
         <div className={styles.container} >
             <h2 style={{color:'white', textAlign:'center'}}>Agrega un Producto</h2>
-        <form style={{display:'flex', flexDirection:'column', gap:'10px'}}>
+        <form style={{display:'flex', flexDirection:'column'}}>
             <input className={styles.input} type="text" placeholder="Nombre Producto" onChange={handleChange} name={"nombre"} required />
             <textarea rows={3} className={styles.input2}  type="text" placeholder="Descripcion Producto" onChange={handleChange} name={"descripcion"} required></textarea>
-            <div style={{display:'flex', flexDirection:'row', gap:'10px'}}>
-            <input className={styles.input} type="text" placeholder="Precio" onChange={handleChange} name={"precio"} required/>
+            <div style={{display:'flex', flexDirection:'row'}}>
+            <input style={{marginRight:'10px'}} className={styles.input} type="text" placeholder="Precio" onChange={handleChange} name={"precio"} required/>
 
-            <input className={styles.input}type="number" placeholder="Cantidad" onChange={handleChange} name={"unidades"}required min={0}/>
+            <input style={{marginRight:'10px'}} className={styles.input}type="number" placeholder="Cantidad" onChange={handleChange} name={"unidades"}required min={0}/>
             <input className={styles.input}type="number" placeholder="Referencia" onChange={handleChange} name={"referencia"}required min={0}/>
-
             </div>
+            <button  style={{width:'100%', color:'white', marginLeft:0, marginTop:0, backgroundColor:'#b680f4'}} className={styles1.button}>Añadir Producto</button>
 
         </form>
         </div>
