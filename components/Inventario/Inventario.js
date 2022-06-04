7622b3a88b94758035227f433b3384adc88785cb
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-04 11:15:54 -0500

    x2

--- components/Inventario/Inventario.js
+++ components/Inventario/Inventario.js
@@ -4,11 +4,13 @@ export default function Inventario({el}){
     console.log(el);
     return(
         <>
+        <div>
         <h2>Bienvenida al Inventario</h2>
         <h2 style={{color:"white"}}><strong>Nombre:</strong>{el.nombre}</h2>
         <h2><b>Descripcion</b>{el.descripcion}</h2>
         <h2><b>Precio</b>{el.precio}</h2>
         <h2><b>Unidades</b>{el.unidades}</h2>
+        </div>
 
 
         </>
