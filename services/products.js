30d30bdc4f60b33870f531e0ebc44266009f0599
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-07 18:56:43 -0500

    x19

--- services/products.js
+++ services/products.js
@@ -2,29 +2,29 @@
 
 export const products=[
     {
-        "nombre": "Collar Cactus",
-        "descripcion":"Desde el lejano oeste Mrs. T ha encontrado cactus unicos, encadenandolos a su cuello, para compartirlos contigo.",
-        "precio":"15.000",
-        "unidades":"3",
-        "referencia":"1234",
-        "id":1
+        nombre: "Collar Cactus",
+        descripcion:"Desde el lejano oeste Mrs. T ha encontrado cactus unicos, encadenandolos a su cuello, para compartirlos contigo.",
+        precio:"15.000",
+        unidades:"3",
+        referencia:"1234",
+        id:1
     },
     {
-        "nombre":"Collar Osito",
-        "descripcion":"Un ataque de gomitas infernal aplasto el planeta, la serpiente las a capturado en sus collares",
-        "precio":"13.000",
-        "unidades":"2",
-        "referencia":"2131",
-        "id":2
+        nombre:"Collar Osito",
+        descripcion:"Un ataque de gomitas infernal aplasto el planeta, la serpiente las a capturado en sus collares",
+        precio:"13.000",
+        unidades:"2",
+        referencia:"2131",
+        id:2
 
     },
     {
-        "nombre":"Collar Palmera",
-        "descripcion":"Desde las paradisíacas playas de madagascar llegan los collares de Palmera ",
-        "precio":"12.000",
-        "unidades":"5",
-        "referencia":"2323",
-        "id":3
+        nombre:"Collar Palmera",
+        descripcion:"Desde las paradisíacas playas de madagascar llegan los collares de Palmera ",
+        precio:"12.000",
+        unidades:"5",
+        referencia:"2323",
+        id:3
 
     }
 
