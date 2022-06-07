195d93eae369265e7fea4bf29e50f241939f6a1e
SantaNaN23 <salazarmiguelito23@gmail.com>
2022-06-07 13:36:32 -0500

    x2

--- services/getOnePost.js
+++ services/getOnePost.js
@@ -1,7 +1,7 @@
 import { products } from "./products";
 
 export default async function getOnePost(){
-    const res = await fetch(`http://localhost:3000/api/products`)
+    const res = await fetch(`https://pythonjoyas.vercel.app/api/products`)
     const posts = await res.json()
     return posts
 }
